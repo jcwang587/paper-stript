840b1a8d1c35817596e1467f18ffde644c8891a4
Alison Ding <96399900+alisonding357@users.noreply.github.com>
2021-12-23 12:38:52 -0800

    Add files via upload

---
+++ read_video.m
@@ -0,0 +1,19 @@
+videoFileReader = VideoReader('paperStrip.mp4');
+depVideoPlayer = vision.DeployableVideoPlayer;
+frame_count = 1;
+disp('Height (pixels) =');
+disp(videoFileReader.Height);
+disp('Width (pixels) =');
+disp(videoFileReader.Width)
+
+while hasFrame(videoFileReader)
+    videoFrame = readFrame(videoFileReader);
+    depVideoPlayer(videoFrame);
+    pause(1/videoFileReader.FrameRate);
+    frame_count = frame_count + 1;
+end 
+release(videoPlayer);
+
+disp('Frame count =');
+disp(frame_count);
+
